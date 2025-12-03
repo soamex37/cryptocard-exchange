65f2045504d45f9e3d6fa2026b892a21574123b1
soamex37 <ismailsoa37@gmail.com>
2025-12-02 21:09:24 -0500

    Create App.js

---
+++ admin/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{fontFamily: "Arial", padding: "40px"}}>
+      <h2>CryptoCard Admin Panel</h2>
+      <p>Login page coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
