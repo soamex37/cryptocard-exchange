a219e02350790f1acdb65abe38eca5ee0cba0465
soamex37 <ismailsoa37@gmail.com>
2025-12-02 23:13:53 -0500

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("CryptoCard Exchange Backend Running Successfully 🚀");
+});
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
