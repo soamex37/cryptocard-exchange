4a5183d3c2a8956243e1ab7d96ac543c07c219b5
soamex37 <ismailsoa37@gmail.com>
2025-12-02 22:33:59 -0500

    Delete backend directory

--- backend/backend/src/config/backend/src/backend/src/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "user" }
-});
-
-export default mongoose.model("User", userSchema);
